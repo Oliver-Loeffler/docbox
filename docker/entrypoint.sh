2ce9e0f5f4a409cf1b3dc3e6d97bc5e790c333cf
Oliver-Loeffler <Oliver-Loeffler@users.noreply.github.com>
2023-08-06 22:46:17 +0200

    First working docker image (in dev mode).

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
 /usr/sbin/php-fpm
-/usr/sbin/httpd -DFOREGROUND
+/usr/sbin/httpd
+# /usr/local/bin/java -jar /docdrop/target/*-runner.jar
+cd /docdrop && sh ./mvnw compile quarkus:dev
+
--- docker/index.php
+++ docker/index.php
@@ -1,1 +1,10 @@
-<?php phpinfo(); ?>
+<<!doctype html>
+<html>
+<head>
+    <meta http-equiv="refresh" content="0; URL=http://localhost:8080/upload.html">
+    <?php phpinfo(); ?>
+</head>
+</html>
+
+
+
