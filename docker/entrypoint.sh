c4350456cf0f320e78152cfdaf200ef2945388c5
Oliver-Loeffler <Oliver-Loeffler@users.noreply.github.com>
2025-07-10 21:05:24 +0200

    fix: Directories artifacts and ingest are now created if not existing.

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -4,5 +4,9 @@
 /usr/local/bin/java -jar /docbox/PrepareHttpd.jar docker
 /usr/local/bin/java -jar /docbox/PrepareDist.jar docker
 
+# Generate Directories if not existing
+mkdir -p /var/www/html/artifacts
+mkdir -p /var/www/html/ingest
+
 # Launching the Services
 (/usr/sbin/httpd; cd /docbox; /usr/local/bin/java -jar /docbox/quarkus-run.jar)
