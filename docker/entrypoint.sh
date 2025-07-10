9f749ca3483ca56797ecd0efd18ff53f4aa3ade1
Oliver-Loeffler <Oliver-Loeffler@users.noreply.github.com>
2025-07-10 21:02:35 +0200

    fix: Replaced CRLF line endings with LF.

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
+
+# Initial Configuration Bootstrapping
 /usr/local/bin/java -jar /docbox/PrepareHttpd.jar docker
 /usr/local/bin/java -jar /docbox/PrepareDist.jar docker
-/usr/sbin/httpd
-(cd /docbox; /usr/local/bin/java -jar /docbox/quarkus-run.jar)
+
+# Launching the Services
+(/usr/sbin/httpd; cd /docbox; /usr/local/bin/java -jar /docbox/quarkus-run.jar)
