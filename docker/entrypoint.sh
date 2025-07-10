bea65abe2d08d52e0604448afc952df275560a5d
Oliver-Loeffler <Oliver-Loeffler@users.noreply.github.com>
2025-07-10 21:01:06 +0200

    Added gitattributes file so that *.sh files no longer break by accident.

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -2,4 +2,4 @@
 /usr/local/bin/java -jar /docbox/PrepareHttpd.jar docker
 /usr/local/bin/java -jar /docbox/PrepareDist.jar docker
 /usr/sbin/httpd
-/usr/local/bin/java -jar /docbox/quarkus-run.jar
+(cd /docbox; /usr/local/bin/java -jar /docbox/quarkus-run.jar)
